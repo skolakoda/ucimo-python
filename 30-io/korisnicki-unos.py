e1e4838724b86a53b02e465b7319f3a1b6758d39
Damjan Pavlica <mudroljub@gmail.com>
2018-05-29 14:56:19 +0200

    novo

---
+++ 30-io/korisnicki-unos.py
@@ -0,0 +1,7 @@
+print("Unesi broj, decimalni broj i tekst:")
+
+broj = int(input()) # read int
+decimala = float(input()) # read double
+tekst = input() # read string
+
+print("Uneli ste:", broj, decimala, tekst)
